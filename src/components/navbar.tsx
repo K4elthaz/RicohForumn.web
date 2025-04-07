317f7cbbca85e83a04a06516bbd8bcf2b82b7e38
HustlinKael <jalcantarabedtc@gmail.com>
2025-04-07 11:30:41 +0800

    update it as personal

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <Link to="/" className="mr-6 flex items-center space-x-2">
-        <img src="/Ricoh_logo_2012.png" alt="Ricoh" className="h-9 w-100" />
+        <img src="/2.png" alt="Ricoh" className="h-9 w-100" />
         </Link>
         <nav className="flex flex-1 items-center space-x-6 text-sm font-medium">
           <Link to="/gallery" className="transition-colors hover:text-primary">
